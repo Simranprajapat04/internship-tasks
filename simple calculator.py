6ff57667e9a6da374ec51a5a56b89b7fdd628d00
Simran prajapati <prajapatisimran557@gmail.com>
2025-02-26 12:27:30 +0530

    Add files via upload
    Task-1 To-do-list #codsoft
    Task-2 simple calculator #codsoft

---
+++ simple calculator.py
@@ -0,0 +1,99 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================================== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python312/simple calculator.py ===================================
+enter first number here: 12
+enter second number here: 3
+press 1 for addition
+press 2 for subtraction
+press 3 for multiplication
+press 4 for division
+
+=================================== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python312/simple calculator.py ===================================
+enter first number here: 4
+enter second number here: 9
+press 1 for addition
+press 2 for subtraction
+press 3 for multiplication
+press 4 for division
+enter your choice from 1-4: 3
+36.0
+
+=================================== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python312/simple calculator.py ===================================
+enter first number here: 3
+enter second number here: 7
+press 1 for addition
+press 2 for subtraction
+press 3 for multiplication
+press 4 for division
+enter your choice from 1-4: 2
+-4.0
+
+======================================= RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python312/To-do list.py ======================================
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice3
+Tasks:
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice3
+Tasks:
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice2
+Enter task to remove 
+Task not found
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice
+invalid choice
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice
+invalid choice
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice
+invalid choice
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice
+======================================= RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python312/To-do list.py ======================================
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice1
+Enter task:breakfasy
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice1
+Enter task:breakfast
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
+Enter your choice3
+Tasks:
+-breakfasy
+-breakfast
+1.Add task
+2.Remove task
+3. Show tasks
+4. quit
